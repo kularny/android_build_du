fb8205949183c94af75b480b5a6ab60266663ab5
Arnab Chaudhuri <coolguyforgenerationx@gmail.com>
2015-09-25 10:53:52 +0530

    Fall back to the default legacy kernel toolchain path

--- envsetup.sh
+++ envsetup.sh
@@ -174,7 +174,7 @@ function setpaths()
     case $ARCH in
         arm)
             # Legacy toolchain configuration used for ARM kernel compilation
-            toolchaindir=arm/arm-cortex-a9/bin
+            toolchaindir=arm/arm-eabi-$targetgccversion/bin
             if [ -d "$gccprebuiltdir/$toolchaindir" ]; then
                  export ARM_EABI_TOOLCHAIN="$gccprebuiltdir/$toolchaindir"
                  ANDROID_KERNEL_TOOLCHAIN_PATH="$ARM_EABI_TOOLCHAIN":
